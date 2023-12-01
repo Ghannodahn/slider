33f01fd8761bb36fbbdabd874e747d500a305d0b
Joe Allan Muharsky <jmuharsky@gmail.com>
2023-12-01 15:01:18 -0800

    Updated whitespace.

--- src/sql/slidify/tables.sql
+++ src/sql/slidify/tables.sql
@@ -1,8 +1,8 @@
-CREATE TABLE EventSession (
-  sessionId SERIAL PRIMARY KEY,
-  startTime TIMESTAMP,
-  endTime   TIMESTAMP
-  );
+  CREATE TABLE EventSession (
+    sessionId SERIAL PRIMARY KEY,
+    startTime TIMESTAMP,
+    endTime   TIMESTAMP
+    );
 
-INSERT INTO EventSession (startTime, endTime) values ('2024-11-24 7:00 PM', '2024-11-24 11:00 PM');
-INSERT INTO EventSession (startTime, endTime) values ('2024-11-26 7:00 PM', '2024-11-26 11:30 PM');
+  INSERT INTO EventSession (startTime, endTime) values ('2024-11-24 7:00 PM', '2024-11-24 11:00 PM');
+  INSERT INTO EventSession (startTime, endTime) values ('2024-11-26 7:00 PM', '2024-11-26 11:30 PM');
