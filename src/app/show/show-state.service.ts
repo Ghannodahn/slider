8cf3bdd0dfb29887239f468db069e02cd300c304
Joe Allan Muharsky <joemu@highlandscastleholdings.com>
2023-12-07 21:15:54 -0800

    Added NextPerormer to Show mode

--- src/app/show/show-state.service.ts
+++ src/app/show/show-state.service.ts
@@ -10,12 +10,25 @@ export class ShowStateService {
     private performersService: PerformersService
   ) { }
 
+  performers: Performer[] = [];
   currentPerformer: Performer = EmptyPerformer;
 
   refresh() {
-    this.performersService.getPerformer(1)
-      .subscribe((performer) => {
-        this.currentPerformer = performer;
+    this.performersService.listPerformers(1)
+      .subscribe((performers) => {
+        this.performers = performers;
+        this.currentPerformer = performers[0];
       });
+  }
+
+  public get nextPerformer(): Performer | null {
+    let selectedIndex = this.performers.indexOf(this.currentPerformer);
+
+    if (selectedIndex === this.performers.length - 1) {
+      // Last Item
+      return null;
+    } else {
+      return this.performers[++selectedIndex];
+    }
   }
 }
